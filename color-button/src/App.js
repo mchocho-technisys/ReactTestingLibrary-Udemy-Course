a21140db9dece908ede13f859befc3b9c165bd9f
mchocho-technisys <mchocho@technisys.com>
2023-09-12 09:02:46 -0300

    Button who changes its color and text and tests

--- color-button/src/App.js
+++ color-button/src/App.js
@@ -1,9 +1,21 @@
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [backgroundColor, setBackgroundColor] = useState('red');
+  const [text, setText] = useState('Change to blue');
+
   return (
     <div>
-      <button style={{ backgroundColor: 'red'}}>Change to blue</button>
+      <button 
+        style={{ backgroundColor: backgroundColor}} 
+        onClick={() => {
+          backgroundColor === 'red' ? setBackgroundColor('blue') : setBackgroundColor('red');
+          text === 'Change to blue' ? setText('Change to red') : setText('Change to blue');
+        }}
+      >
+        {text}
+      </button>
     </div>
   );
 }
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,12 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { logRoles } from '@testing-library/react';
 import App from './App';
 
 test('button has correct initial color', () => {
-  render(<App />);
+  const { container } = render(<App />);
+  // Provide a console log of the Component
+  logRoles(container);
 
   const colorButton = screen.getByRole('button', { name: 'Change to blue'});
 
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
 
-test('button turns blue when clicked', () => {});
+test('button has correct initial text', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+
+  expect(colorButton).toHaveTextContent('Change to blue');
+});
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+test('button changes text when clicked', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  fireEvent.click(colorButton);
+  expect(colorButton).toHaveTextContent('Change to red');
+});
